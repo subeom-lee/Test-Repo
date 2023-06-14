6c1c8d59e67b62e5086beea722cb94186df124af
Su Beom Lee 이수범 주임 소프트피플코리아 <subeom1.lee@doosan.com>
2023-06-14 11:15:27 +0900

    초기 커밋.

---
+++ Test GithubActions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_GithubActions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
